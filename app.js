7659111ee3a6d090fe233d7e021f615115a251fe
Omar622 <omaramin622@gmail.com>
2023-05-24 07:45:35 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const createError = require('http-errors');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// Set up mongoose connection
+mongoose.set("strictQuery", false);
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.DB_CONNECTION_STR);
+}
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // send the error message
+  res.status(err.status || 500);
+  res.json({ message: 'something went wrong' });
+});
+
+module.exports = app;
