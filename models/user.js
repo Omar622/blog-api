371c0936c5e0de036eca2c483f27afc7f9f30666
Omar622 <omaramin622@gmail.com>
2023-05-24 08:49:10 +0300

    Update user model
    user email instead of username

--- models/user.js
+++ models/user.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true, maxLength: 100 },
+  email: {
+    type: String, required: true, unique: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
   password: { type: String, required: true, maxLength: 100 },
   first_name: { type: String, required: true, maxLength: 100 },
   last_name: { type: String, required: true, maxLength: 100 },
